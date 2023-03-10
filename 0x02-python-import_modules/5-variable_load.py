01bdd984c7591b0d10971e933cd50e379706c71b
NgethaWachira <jngethawachira@gmail.com>
2023-03-10 09:30:42 -0800

    importing the variable a from the file variable_load_5.py and prints its value

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """importing  variable a from the file variable_load_5.py and prints its value."""
+
+    import variable_load_5
+
+    new_var = variable_load_5.a
+
+    print(new_var)
