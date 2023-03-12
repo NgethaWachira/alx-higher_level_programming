9a62706f43c65fb45543381ec90b295473ae62d4
NgethaWachira <jngethawachira@gmail.com>
2023-03-12 01:34:45 -0800

    added an if statement to check if list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for nums in my_list:
-        nums.reverse()
-        print("{:d}".format(nums))
+    """ if statement checks if the list of intergers is empty."""
+    if not my_list:
+        pass 
+
+    else:
+        for nums in my_list:
+            nums.reverse()
+            print("{:d}".format(nums))
