b3b139f43049291a4ea90dbaa6be7d0b8d204465
NgethaWachira <jngethawachira@gmail.com>
2023-03-18 06:34:39 -0700

    tweaking code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@ def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
 
     for new in new_dict.keys():
-        new_dict[new] = new_dict * 2
+        new_dict[new] *= 2
         return new_dict
