7cf946b593bda76703a33c0a68c766c1899241b5
NgethaWachira <jngethawachira@gmail.com>
2023-03-18 03:07:22 -0700

    added the sum function

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     list_set = set(my_list)
     """ converting the list set to a list."""
 
-    unique_list = list(list_set)
+    unique_list = sum(list(list_set))
 
     result = uniq_add(unique_list)
     print(result)
