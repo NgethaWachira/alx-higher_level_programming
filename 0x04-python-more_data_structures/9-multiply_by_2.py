651e63c43a8791b01dba446a32ac572915264970
NgethaWachira <jngethawachira@gmail.com>
2023-03-18 06:49:45 -0700

    multiplied the values instead of keys

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
 
-    for new in new_dict.keys():
-        new_dict[new] *= 2
+    for keys, values in new_dict.items():
+        new_dict[values] *= 2
         return new_dict
