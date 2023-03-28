41cec742c3d0679a5e9047ae884b3a4505336983
NgethaWachira <jngethawachira@gmail.com>
2023-03-27 23:40:21 -0700

    misspelling of prinbt statements

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,13 +7,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("Wrong type")
+            print("wrong type")
             result = 0
         except ZeroDivisionError:
-            print("Division by zero")
+            print("division by 0")
             result = 0
         except IndexError:
-            print("Out of range")
+            print("out of range")
             result = 0
         finally:
             new_list.append(result)
